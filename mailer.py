f0f8d4a1871ae1d7446201e8aa3ab079419a8413
BackgroundLogic <52327130+BackgroundLogic@users.noreply.github.com>
2022-11-03 10:56:20 -0400

    setup mailer functionality, need to set env variables on pythonanywhere but code works, disabled until full site launch
    setup confirmation page for message sent with redirect to homepage

--- mailer.py
+++ mailer.py
@@ -8,16 +8,16 @@ class Mailer:
         self.FROM_EMAIL = os.environ.get("FROM_EMAIL")
         self.EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-    def send_mail(self, name, contact_email, message):
+    def send_mail(self, name, message, email):
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(
                 user=self.FROM_EMAIL,
                 password=self.EMAIL_PASSWORD,
             )
-            to_emails = f"jangolson@outlook.com; {contact_email}"
+            to_emails = f"jangolson@outlook.com; {email}"
             connection.sendmail(
                 from_addr=self.FROM_EMAIL,
                 to_addrs=to_emails,
-                msg=f"Subject:New Message From {name}\n\n{message}"
+                msg=f"Subject:New Message From {name}\n\nEmail: {email}\nMessage: {message}"
             )
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from mailer import Mailer
 
 app = Flask(__name__)
-
+mailer = Mailer()
 
 @app.route('/')
 def home():
@@ -21,6 +21,15 @@ def template():
 
 @app.route('/message_sent', methods=['POST'])
 def message_sent():
+    msg_data = request.form
+    # Below code works in test environment,
+    # need to set env for deployment environment,
+    # keeping disabled until ready for full site launch
+    # mailer.send_mail(
+    #     name=msg_data['name'],
+    #     message=msg_data['message'],
+    #     email=msg_data['email'],
+    # )
     return render_template('message-sent.html')
 
 
